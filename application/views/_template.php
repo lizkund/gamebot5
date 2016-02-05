ca6e1fb3859f5675c307f5f7b6248a9f0f2465ac
josephwithay <yoseph_jo@hotmail.com>
2016-02-05 15:37:25 -0800

    _template.php added Signed-off-by:josephwithay <yoseph_jo@hotmail.com>

--- application/views/_template.php
+++ application/views/_template.php
@@ -3,16 +3,20 @@
 	<head>
 		<title>{pagetitle}</title>
 		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
+		
+		<!-- CSS -->
+
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 	</head>
 	<body>
 		<div id="wrapper">
 			<div id="header">
-				<span class="myhead">Robo Factory</span>
+				<span class="myhead">Coding for Donuts </span>
 				<span class="mynav">
 					<ul>
 						<li><a href="/">Home</a></li>
-						<li><a href="/gallery">Player Info</a></li>
-						<li><a href="/about">Assemble</a></li>
+						<li><a href="/info">Player Info</a></li>
+						<li><a href="/assemble">Assemble</a></li>
 					</ul>
 				</span>
 			</div>
