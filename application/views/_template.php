a7a161a13984a6fbd40443ea5a765c38db6a7eb9
josephwithay <yoseph_jo@hotmail.com>
2016-02-05 15:20:17 -0800

    _template.php added Signed-off-by:josephwithay <yoseph_jo@hotmail.com>

---
+++ application/views/_template.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>{pagetitle}</title>
+		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header">
+				<span class="myhead">Robo Factory</span>
+				<span class="mynav">
+					<ul>
+						<li><a href="/">Home</a></li>
+						<li><a href="/gallery">Player Info</a></li>
+						<li><a href="/about">Assemble</a></li>
+					</ul>
+				</span>
+			</div>
+			<div class="alone">
+
+			</div>
+			<div id="content">
+				{content}
+			</div>
+
+			<div id="footer" class="span12">
+				Copyright &copy; 2016 <strong></strong>.
+			</div>
+		</div>
+	</body>
+</html>
