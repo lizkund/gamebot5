4235c706280c0a0a4c580504dc6075f9f36bbeb3
jotilalli <jotilalli@gmail.com>
2016-02-10 19:45:06 -0800

    Edited the playerinfo view Signed-off-by:jotilalli <jotilalli@gmail.com>

--- application/views/_playerinfo1.php
+++ application/views/_playerinfo1.php
@@ -1,10 +1,5 @@
-
-
-    <tr>
-        <td><a href="{link}">{player}</a></td>
-        <td>{peanuts}</td>
-        <td>{equity}</td>
-    </tr>    
-            
-
-
+<tr>
+    <td><a href="{link}">{player}</a></td>
+    <td>{peanuts}</td>
+    <td>{equity}</td>
+</tr>    
