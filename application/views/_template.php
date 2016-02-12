ca42226a36750a88f29b4a8d37c616d9d8e9574a
josephwithay <yoseph_jo@hotmail.com>
2016-02-12 10:21:08 -0800

    the link to css is changed to what Kenneth suggested Signed-off-by:josephwithay <yoseph_jo@hotmail.com>

--- application/views/_template.php
+++ application/views/_template.php
@@ -3,10 +3,10 @@
 	<head>
 		<title>{title}</title>
 		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
-		<link href="../../css/style.css" rel="stylesheet" type="text/css"/>
-		<link href="../../css/button.css" rel="stylesheet" type="text/css"/>
-		<link href="../../css/tablet.css" rel="stylesheet" type="text/css"/>
-		<link href="../../css/smartphone.css" rel="stylesheet" type="text/css"/>
+		<link href="{css}style.css" rel="stylesheet" type="text/css"/>
+		<link href="{css}button.css" rel="stylesheet" type="text/css"/>
+		<link href="{css}tablet.css" rel="stylesheet" type="text/css"/>
+		<link href="{css}smartphone.css" rel="stylesheet" type="text/css"/>
 	</head>
 	<body>
 		<header>
@@ -28,7 +28,6 @@
 			<p id='footer-in'>Copyright &copy; 2016 <strong>Group 5 - Assignment 1</strong></p>
 		</footer>
 	</body>
-	<!--[if !IE]><!--><script src="https://code.jquery.com/jquery-2.1.4.min.js" type="text/javascript"></script><!--<![endif]-->
+	<!--[if !IE]><!--><script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script><!--<![endif]-->
 	<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js" type="text/javascript"></script>
-	<script src="../../js/script.js" type="text/javascript"></script>
 </html>
