26da5ff8315324791553eb3352b937c7fabd5c3a
josephwithay <yoseph_jo@hotmail.com>
2016-02-12 16:03:17 -0800

    The way title of each page looks has been changed and changelog updated Signed-off-by:josephwithay <yoseph_jo@hotmail.com>

--- application/views/_template.php
+++ application/views/_template.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
-		<title>{title}</title>
+		<title>{pageTitle} | {site-title}</title>
 		<meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
 		<link href="{appRoot}/css/style.css" rel="stylesheet" type="text/css"/>
 		<link href="{appRoot}/css/button.css" rel="stylesheet" type="text/css"/>
