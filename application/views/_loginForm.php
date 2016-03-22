e08cff87a642a30278a490941bfead1962ebc8a3
josephwithay <yoseph_jo@hotmail.com>
2016-03-21 19:16:42 -0700

    Changes made according to the request Signed-off-by:josephwithay <yoseph_jo@hotmail.com>

--- application/views/_loginForm.php
+++ application/views/_loginForm.php
@@ -5,12 +5,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * Login Form
  */
 ?>
-<form class="login" method="POST">
-	<table>
-		<tr>
-			<td><label for="username">Username:</label></td>
-			<td><input type="text" name="username" id="username" class="textbox" maxlength="25" /></td>
-			<td><input type="submit" name="login" value="Login/Register"/></td>
-		</tr>
-	</table>
-</form>
+<div id="loginbar">
+	<form class="login" method="POST">
+		<table>
+			<tr>
+				<td><label for="username">Username:</label></td>
+				<td><input type="text" name="username" id="username" class="textbox" maxlength="25" /></td>
+				<td><input type="submit" name="login" value="Login/Register"/></td>
+			</tr>
+		</table>
+	</form>
+</div>
